65af01f49815b148be4f6bd64d8955887b0f3553
motiejus1 <motiejus.baliukonis@vka.lt>
2022-04-01 18:40:25 +0300

    *vue js integration

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('{any}', function() {
+    //app.blade.php - VueJS framework
+    //
+    return view('app');
+})->where('any', '.*');
